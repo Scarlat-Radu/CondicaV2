48f142ffa1fa48cb967cc9ee86818259998bcc94
Radu <radu.scarlat@allengra.eu>
2022-02-10 13:09:47 +0200

    Add project files.

---
+++ ServerControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for ServerControl.xaml
+    /// </summary>
+    public partial class ServerControl : UserControl
+    {
+        public ServerControl()
+        {
+            InitializeComponent();
+        }
+
+        private void tbComandLine_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void bStartServer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
